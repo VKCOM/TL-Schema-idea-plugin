b2cedc5fb459207b391765a2d1a6ca6b9bd7e07b
Pavel Kunyavskiy <kunyavskiy@gmail.com>
2016-06-21 11:19:48 +0300

    Commenter

---
+++ src/net/pavelk/tlschema/TLSchemaCommenter.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright (C) 2016 Pavel Kunyavskiy
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package net.pavelk.tlschema;
+
+import com.intellij.lang.Commenter;
+import org.jetbrains.annotations.Nullable;
+
+public class TLSchemaCommenter implements Commenter {
+
+    @Nullable
+    @Override
+    public String getLineCommentPrefix() {
+        return "//";
+    }
+
+    @Nullable
+    @Override
+    public String getBlockCommentPrefix() {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public String getBlockCommentSuffix() {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public String getCommentedBlockCommentPrefix() {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public String getCommentedBlockCommentSuffix() {
+        return null;
+    }
+}
