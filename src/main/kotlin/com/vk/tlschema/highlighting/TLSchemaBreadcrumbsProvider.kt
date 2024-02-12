3f409fa34fa220356673d2482f656f7a9a2fa3dd
Yury Barinov <y.barinov@vk.team>
2024-02-12 15:23:20 +0300

    fixup

--- src/main/kotlin/com/vk/tlschema/highlighting/TLSchemaBreadcrumbsProvider.kt
+++ src/main/kotlin/com/vk/tlschema/highlighting/TLSchemaBreadcrumbsProvider.kt
@@ -1,6 +1,5 @@
 package com.vk.tlschema.highlighting
 
-import com.intellij.lang.Language
 import com.intellij.openapi.util.NlsSafe
 import com.intellij.psi.PsiElement
 import com.intellij.ui.breadcrumbs.BreadcrumbsProvider
@@ -8,31 +7,26 @@ import com.vk.tlschema.TLSchemaLanguage
 import com.vk.tlschema.psi.TLSchemaConstructorDeclarations
 import com.vk.tlschema.psi.TLSchemaDeclaration
 import com.vk.tlschema.psi.TLSchemaFunDeclarations
-import java.util.*
 
 class TLSchemaBreadcrumbsProvider : BreadcrumbsProvider {
     override fun getLanguages() = arrayOf(TLSchemaLanguage.INSTANCE)
 
-    override fun acceptElement(psiElement: PsiElement): Boolean {
-        return (psiElement is TLSchemaFunDeclarations
-                || psiElement is TLSchemaConstructorDeclarations
-                || psiElement is TLSchemaDeclaration)
+    override fun acceptElement(element: PsiElement) : Boolean {
+        return when (element) {
+            is TLSchemaFunDeclarations,
+            is TLSchemaConstructorDeclarations,
+            is TLSchemaDeclaration -> true
+
+            else -> false
+        }
     }
 
-    override fun getElementInfo(psiElement: PsiElement): @NlsSafe String {
+    @NlsSafe
+    override fun getElementInfo(psiElement: PsiElement) : String {
         return when (psiElement) {
-            is TLSchemaFunDeclarations -> {
-                "functions"
-            }
-
-            is TLSchemaConstructorDeclarations -> {
-                "types"
-            }
-
-            is TLSchemaDeclaration -> {
-                psiElement.getCombinator().getName()!!
-            }
-
+            is TLSchemaFunDeclarations -> "functions"
+            is TLSchemaConstructorDeclarations -> "types"
+            is TLSchemaDeclaration -> psiElement.getCombinator().getName()!!
             else -> throw IllegalArgumentException("Not supported")
         }
     }
