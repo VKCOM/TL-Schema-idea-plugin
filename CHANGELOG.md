168f2d6372869e2b2358b47dfb9c40c8cfa6d53d
Danil Ovchinnikov <danil42worker@gmail.com>
2024-02-12 11:57:08 +0300

    moved project to Gradle

---
+++ CHANGELOG.md
@@ -0,0 +1,36 @@
+# TL Schema Support Changelog
+
+## [Unreleased]
+
+## 1.4.0 — 12.02.2024
+
+- Moved project to Gradle
+- Change package to `com.vk.tlschema`
+
+## 1.3.2 — 08.07.2020
+
+- Support @kphp annotation. Added plugin icon :)
+
+## 1.3.1 — 25.09.2019
+
+- Fix NPE, some minor improvments Rebuild with java 1.8 for better compatibility with older IDE versions
+
+## 1.3.0 — 18.09.2019
+
+- Fix NPE, some minor improvments
+
+## 1.2.0 — 07.07.2016
+
+- Support of Find Usages and references
+
+## 1.1.0 — 20.09.2015
+
+- Structure view for tl schema files. Fixed bug with optional type variables
+
+## 1.0.1 — 15.09.2015
+
+- Fixed bug with optional type variables
+
+## 1.0.0 — 14.09.2015
+
+- First stable version
---
+++ build.gradle.kts
@@ -0,0 +1,89 @@
+import org.jetbrains.changelog.Changelog
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+fun properties(key: String) = providers.gradleProperty(key)
+fun environment(key: String) = providers.environmentVariable(key)
+
+plugins {
+    id("java") // Java support
+    alias(libs.plugins.kotlin) // Kotlin support
+    alias(libs.plugins.gradleIntelliJPlugin) // Gradle IntelliJ Plugin
+    alias(libs.plugins.changelog) // Gradle Changelog Plugin
+    alias(libs.plugins.grammarkit) // Gradle Grammar-Kit Plugin
+}
+
+group = properties("pluginGroup").get()
+version = properties("pluginVersion").get()
+
+// Configure project's dependencies
+repositories {
+    mavenCentral()
+}
+
+// Dependencies are managed with Gradle version catalog - read more: https://docs.gradle.org/current/userguide/platforms.html#sub:version-catalog
+dependencies {
+//    implementation(libs.annotations)
+}
+
+// Set the JVM language level used to build the project. Use Java 11 for 2020.3+, and Java 17 for 2022.2+.
+kotlin {
+    @Suppress("UnstableApiUsage")
+    jvmToolchain {
+        languageVersion = JavaLanguageVersion.of(17)
+        vendor = JvmVendorSpec.JETBRAINS
+    }
+}
+
+// Configure Gradle IntelliJ Plugin - read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
+intellij {
+    pluginName = properties("pluginName")
+    version = properties("platformVersion")
+    type = properties("platformType")
+
+    // Plugin Dependencies. Uses `platformPlugins` property from the gradle.properties file.
+    plugins = properties("platformPlugins").map { it.split(',').map(String::trim).filter(String::isNotEmpty) }
+}
+
+// Configure Gradle Changelog Plugin - read more: https://github.com/JetBrains/gradle-changelog-plugin
+changelog {
+    groups.empty()
+    repositoryUrl = properties("pluginRepositoryUrl")
+}
+
+sourceSets {
+    main {
+        java.srcDirs("src/main/gen")
+    }
+}
+
+tasks {
+    generateLexer {
+        sourceFile = file("src/main/grammars/TLShcema.flex")
+        targetDir = "src/main/gen/com/vk/tlschema"
+        targetClass = "TLSchemaLexer"
+        purgeOldFiles = true
+    }
+
+    wrapper {
+        gradleVersion = properties("gradleVersion").get()
+    }
+
+    patchPluginXml {
+        version = properties("pluginVersion")
+        sinceBuild = properties("pluginSinceBuild")
+        untilBuild = properties("pluginUntilBuild")
+
+        val changelog = project.changelog // local variable for configuration cache compatibility
+        // Get the latest available change notes from the changelog file
+        changeNotes = properties("pluginVersion").map { pluginVersion ->
+            with(changelog) {
+                renderItem(
+                    (getOrNull(pluginVersion) ?: getUnreleased())
+                        .withHeader(false)
+                        .withEmptySections(false),
+                    Changelog.OutputType.HTML,
+                )
+            }
+        }
+    }
+}
