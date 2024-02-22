72c8f8627827f6acfae43bf1a3b45537935d2b6a
Danil Ovchinnikov <danil42worker@gmail.com>
2024-02-23 00:06:12 +0300

    added lexer tests for comments

---
+++ src/test/kotlin/com/vk/tlschema/lexer/TLSchemaLexerTestBase.kt
@@ -0,0 +1,22 @@
+package com.vk.tlschema.lexer
+
+import com.intellij.testFramework.LexerTestCase
+import com.vk.tlschema.TLSchemaLexerAdapter
+import com.vk.tlschema.TLSchemaTestingUtil
+
+abstract class TLSchemaLexerTestBase : LexerTestCase() {
+    override fun createLexer() = TLSchemaLexerAdapter()
+
+    override fun getDirPath() = "${TLSchemaTestingUtil.getTestDataPath()}/lexer"
+
+    override fun getPathToTestDataFile(extension: String): String {
+        return dirPath + "/" + getTestName(false) + extension
+    }
+
+    override fun getTestName(lowercaseFirstLetter: Boolean): String {
+        val name = super.getTestName(lowercaseFirstLetter)
+        return name.trimStart()
+    }
+
+    fun doTest() = super.doFileTest("tl")
+}
