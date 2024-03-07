e9a3b8cb7183854c6eb3b4c23f2b0086b7ead5a8
Danil Ovchinnikov <danil42worker@gmail.com>
2024-03-07 23:56:30 +0300

    use `fileType` instead of the deprecated `fileTypeFactory`

--- src/main/kotlin/com/vk/tlschema/TLSchemaFileType.kt
+++ src/main/kotlin/com/vk/tlschema/TLSchemaFileType.kt
@@ -4,7 +4,7 @@ import com.intellij.openapi.fileTypes.LanguageFileType
 import javax.swing.Icon
 
 object TLSchemaFileType : LanguageFileType(TLSchemaLanguage.INSTANCE) {
-    override fun getName(): String = "TL schema file"
+    override fun getName(): String = "TLSchema"
 
     override fun getDescription(): String = "TL schema file"
 
