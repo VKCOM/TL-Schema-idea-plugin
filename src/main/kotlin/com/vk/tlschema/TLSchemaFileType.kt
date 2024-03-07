cfa8d4af17f51245159fd8877d0371ca5cb17884
Danil Ovchinnikov <danil42worker@gmail.com>
2024-03-07 23:49:19 +0300

    convert `TLSchemaFileType` class from `Java` to `Kotlin`

---
+++ src/main/kotlin/com/vk/tlschema/TLSchemaFileType.kt
@@ -0,0 +1,14 @@
+package com.vk.tlschema
+
+import com.intellij.openapi.fileTypes.LanguageFileType
+import javax.swing.Icon
+
+object TLSchemaFileType : LanguageFileType(TLSchemaLanguage.INSTANCE) {
+    override fun getName(): String = "TL schema file"
+
+    override fun getDescription(): String = "TL schema file"
+
+    override fun getDefaultExtension(): String = "tl"
+
+    override fun getIcon(): Icon = TLSchemaIcons.FILE
+}
