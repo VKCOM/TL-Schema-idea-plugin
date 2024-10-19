c2dac63900545d03d195a7be1f9b8a3fca04e851
Danil Ovchinnikov <danil42worker@gmail.com>
2024-10-20 01:17:37 +0300

    `TLSchemaPsiImplUtil` is marked as `Deprecated`

--- src/main/java/com/vk/tlschema/psi/impl/TLSchemaPsiImplUtil.java
+++ src/main/java/com/vk/tlschema/psi/impl/TLSchemaPsiImplUtil.java
@@ -17,6 +17,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
+@Deprecated
 public class TLSchemaPsiImplUtil {
     @Nullable
     public static TLSchemaLcIdentNs getCombinator(TLSchemaDeclaration decl) {
