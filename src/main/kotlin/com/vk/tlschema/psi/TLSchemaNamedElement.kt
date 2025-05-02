bb0ba06ec187ed0dbd9669588595e700d415f35c
Danil Ovchinnikov <danil42worker@gmail.com>
2025-05-02 16:23:13 +0300

    removed the override of autogen functions

--- src/main/kotlin/com/vk/tlschema/psi/TLSchemaNamedElement.kt
+++ src/main/kotlin/com/vk/tlschema/psi/TLSchemaNamedElement.kt
@@ -1,19 +1,6 @@
 package com.vk.tlschema.psi
 
-import com.intellij.navigation.ItemPresentation
 import com.intellij.navigation.NavigationItem
-import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiNamedElement
-import com.intellij.psi.PsiReference
 
-interface TLSchemaNamedElement : TLSchemaElement, PsiNamedElement, NavigationItem {
-    override fun getName(): String?
-
-    override fun setName(newName: String): PsiElement
-
-    override fun getReference(): PsiReference?
-
-    override fun getReferences(): Array<PsiReference>
-
-    override fun getPresentation(): ItemPresentation?
-}
+interface TLSchemaNamedElement : TLSchemaElement, PsiNamedElement, NavigationItem
