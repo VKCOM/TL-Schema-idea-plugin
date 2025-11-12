b448f5fba32ee8e5cf58217c12c81a08da43a6fd
Danil Ovchinnikov <danil42worker@gmail.com>
2025-11-12 23:47:53 +0300

    updated project dependencies (#41)

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,7 +35,6 @@ repositories {
 dependencies {
     testImplementation(libs.junit)
     testImplementation(libs.opentest4j)
-    testImplementation(libs.konsist)
 
     // IntelliJ Platform Gradle Plugin Dependencies Extension - read more: https://plugins.jetbrains.com/docs/intellij/tools-intellij-platform-gradle-plugin-dependencies-extension.html
     intellijPlatform {
@@ -46,6 +45,9 @@ dependencies {
 
         // Plugin Dependencies. Uses `platformPlugins` property from the gradle.properties file for plugin from JetBrains Marketplace.
         plugins(providers.gradleProperty("platformPlugins").map { it.split(',') })
+
+        // Module Dependencies. Uses `platformBundledModules` property from the gradle.properties file for bundled IntelliJ Platform modules.
+        bundledModules(providers.gradleProperty("platformBundledModules").map { it.split(',') })
 
         testFramework(TestFrameworkType.Platform)
     }
@@ -80,12 +82,6 @@ intellijPlatform {
 
         ideaVersion {
             sinceBuild = providers.gradleProperty("pluginSinceBuild")
-
-            // We want to support all future versions, so we need to use a null provider here.
-            // Without this, we cannot pass the `Plugin Verifier`.
-            val pluginUntilBuild = providers.gradleProperty("pluginUntilBuild")
-                .takeUnless { it.orNull.isNullOrBlank() } ?: provider { null }
-            untilBuild = pluginUntilBuild
         }
     }
 
